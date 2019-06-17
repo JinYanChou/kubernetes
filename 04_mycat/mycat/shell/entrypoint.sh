3b00d074214874de1f35ea0d6ac0e89f3bb4c80a
JinYan <jinyan@cathaylife.com.tw>
2019-06-17 10:49:45 +0800

    update

--- 04_mycat/mycat/shell/entrypoint.sh
+++ 04_mycat/mycat/shell/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-SERVER_CONF=/mycat/conf/server.xml
-if [ ! -f "$SERVER_CONF" ]; then
+# SERVER_CONF=/mycat/conf/server.xml
+# if [ ! -f "$SERVER_CONF" ]; then
     cp -r /root/conf /mycat
-fi
+    cp /root/rule.xml /mycat/conf/rule.xml
+    cp /root/schema.xml /mycat/conf/schema.xml
+    cp /root/server.xml /mycat/conf/server.xml
+# fi
 
 for mysqlServer in `host mysql.test-namespace.svc.cluster.local | awk '{print $4}'`
 do
