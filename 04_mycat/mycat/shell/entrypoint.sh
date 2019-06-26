ee907e1c8c14ee122c44e2934f43e2b137f69b4b
JinYan <jinyan@cathaylife.com.tw>
2019-06-26 11:27:34 +0800

    update

--- 04_mycat/mycat/shell/entrypoint.sh
+++ 04_mycat/mycat/shell/entrypoint.sh
@@ -6,10 +6,8 @@ cp /root/server.xml /mycat/conf/server.xml
 
 for mysqlServer in `host mysql.test-namespace.svc.cluster.local | awk '{print $4}'`
 do
-    ./mysql-db-create.sh dbag ebaf ebaf $mysqlServer
-    ./mysql-db-create.sh dbab ebaf ebaf $mysqlServer
-    ./mysql-table-create.sh dbag $mysqlServer
-    ./mysql-table-create.sh dbab $mysqlServer
+    ./mysql-db-create.sh mycat ebaf ebaf $mysqlServer
+    ./mysql-table-create.sh mycat $mysqlServer
 done
 
 /mycat/bin/mycat console
